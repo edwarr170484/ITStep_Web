dae30e428818c3bafb45b87dab6f3103a8b57ac1
Edgar Zhinevski <edwarr170484@gmail.com>
2025-03-16 19:42:16 +0300

    Добавил игровую механику

--- projects/games/professions/js/professions.js
+++ projects/games/professions/js/professions.js
@@ -1,3 +1,8 @@
+let settings = {
+    total: 6,
+    timeout: 2000
+}
+
 class Pair {
     constructor(id, man, job) {
         this.id = id;
@@ -6,116 +11,222 @@ class Pair {
     }
 }
 
+class Result {
+    constructor() {
+        this.man = null;
+        this.job = null;
+        this.score = 0;
+        this.current = null;
+    }
+
+    getMan() {
+        return this.man;
+    }
+
+    getJob() {
+        return this.job;
+    }
+
+    setMan(man) {
+        this.man = man;
+
+        if(!this.job) {
+            man.resetEventListeners();
+            document.getElementById("game-left").append(man.element);
+        }
+    }
+
+    setJob(job) {
+        this.job = job;
+
+        if(!this.man) {
+            this.job.resetEventListeners();
+            document.getElementById("game-right").append(this.job.element);
+        }
+    }
+
+    compare() {
+        if(this.man && this.job) {
+            if(this.man.pair.id == this.job.pair.id){
+                alert("Все верно!");
+                this.score++;
+                this.man.remove();
+                this.job.remove();
+                this.reset();
+
+                if(this.score == settings.total) {
+                    game.over();
+                }
+            } else {
+                alert("Выберите другой вариант");
+                this.current.back();
+            }
+        }
+    }
+
+    reset() {
+        this.man = null;
+        this.job = null;
+        this.current = null;
+    }
+}
+
 class Element {
     constructor(pair, prop) {
         this.pair = pair;
         this.image = pair[prop];
-
-        this.variant = document.createElement('div');
-        this.variant.classList.add('variant');
-        this.variant.innerHTML = pair[prop];
-        this.variant.pair = pair;
-
-        this.getCoords = () => {   
-            let box = this.variant.getBoundingClientRect();
-            
-            return {
-              top: box.top + window.scrollY,
-              left: box.left + window.scrollX
-            };
-        }
-
-        this.moveAt = (e, shiftX, shiftY) => {
-            this.variant.style.left = e.pageX - shiftX + 'px';
-            this.variant.style.top = e.pageY - shiftY + 'px';
-        }
+        this.type = prop;
+        this.element = document.createElement('div');
+        this.coords = [];
 
         this.initEventListeners();
+    }
+
+    getCoords() {   
+        let box = this.element.getBoundingClientRect();
+        
+        return {
+          top: box.top + window.scrollY,
+          left: box.left + window.scrollX
+        };
+    }
+
+    moveAt(e, shiftX, shiftY) {
+        this.element.style.left = e.pageX - shiftX + 'px';
+        this.element.style.top = e.pageY - shiftY + 'px';
     }
     
     getDom() {
         let dom = document.createElement('div');
         dom.classList.add('game-variant');
-        dom.append(this.variant);
+
+        this.element.classList.add('variant');
+        this.element.innerHTML = this.image;
+
+        dom.append(this.element);
 
         return dom;
     }
 
     initEventListeners() {
-        this.variant.ondragstart = function() {
+        this.element.ondragstart = function() {
             return false;
         }
 
-        this.variant.onmousedown = (e) => {
+        this.element.onmousedown = (e) => {
+            this.element.style.transition = 'none';
             let coords = this.getCoords();
             let shiftX = e.pageX - coords.left;
             let shiftY = e.pageY - coords.top;
 
-            this.variant.style.position = 'absolute';
-            document.body.appendChild(this.variant);
+            this.element.style.position = 'absolute';
+            document.body.appendChild(this.element);
 
             this.moveAt(e, shiftX, shiftY);
 
-            this.variant.style.zIndex = 1000; 
+            this.element.style.zIndex = 1000; 
 
             document.onmousemove = (e) => {
                 this.moveAt(e, shiftX, shiftY);
             };
 
-            this.variant.onmouseup = () => {
-                document.onmousemove = null;
-                this.variant.onmouseup = null;
+            this.element.onmouseup = () => {
+                this.resetEventListeners();
+                game.result.current = this;
+
+                switch(this.type) {
+                    case 'man':
+                        if(!game.result.getMan()) {
+                            game.result.setMan(this);
+                        } else {
+                            this.back();
+                        }
+                    break;
+
+                    case 'job':
+                        if(!game.result.getJob()) {
+                            game.result.setJob(this);
+                        } else {
+                            this.back();
+                        }
+                        
+                    break;
+                }
+
+                game.result.compare();
             };
         }
+    }
+
+    resetEventListeners() {
+        document.onmousemove = null;
+        this.element.onmousedown = null;
+        this.element.onmouseup = null;
+    }
+
+    back() {
+        this.element.style.transition = 'all 0.3s linear';
+        this.element.style.left = this.coords.left + 'px';
+        this.element.style.top = this.coords.top + 'px';
+    }
+
+    remove() {
+        this.element.remove();
     }
 }
 
 class Game {
-    constructor(mans, jobs) {
-        this.mans = mans;
-        this.jobs = jobs;
-        this.score = 0;
+    constructor() {
+        this.pairs = [];
+        this.mans = [];
+        this.jobs = [];
         this.variants = [];
+        this.result = new Result();
     }
 
     init() {
+        this.pairs = [
+            new Pair(1, 1, 1),
+            new Pair(2, 2, 2),
+            new Pair(3, 3, 3),
+            new Pair(4, 4, 4),
+            new Pair(5, 5, 5),
+            new Pair(6, 6, 6)
+        ];
+        
+        this.mans = this.pairs.map((pair) => {
+            return new Element(pair, 'man');
+        });
+        
+        this.mans.shuffle();
+        
+        this.jobs = this.pairs.map((pair) => {
+            return new Element(pair, 'job');
+        });
+        
+        this.jobs.shuffle();
+
         this.score = 0;
         this.drawCards(this.mans, document.getElementById('mans-cards'))
             .drawCards(this.jobs, document.getElementById('jobs-cards'));
     }
 
     drawCards(items, field) {
-        items.forEach((item) => {
+        items.forEach((item, index) => {
             field.append(item.getDom());
+            items[index].coords = item.getCoords();
 
             this.variants.push(item.variant);
         });
 
         return this;
     }
+
+    over() {
+        document.getElementById("game-over").classList.add("active");
+    }
 }
 
-let pairs = [
-    new Pair(1, 1, 1),
-    new Pair(2, 2, 2),
-    new Pair(3, 3, 3),
-    new Pair(4, 4, 4),
-    new Pair(5, 5, 5),
-    new Pair(6, 6, 6)
-];
-
-let mans = pairs.map((pair) => {
-    return new Element(pair, 'man');
-});
-
-mans.shuffle();
-
-let jobs = pairs.map((pair) => {
-    return new Element(pair, 'job');
-});
-
-jobs.shuffle();
-
-let game = new Game(mans, jobs);
+let game = new Game();
 
 game.init();
