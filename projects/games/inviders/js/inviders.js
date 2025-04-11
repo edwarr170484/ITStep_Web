caea4a826037d1a209b30b70da43e358b19e6eef
Edgar Zhinevski <edwarr170484@gmail.com>
2025-04-11 18:43:46 +0300

    добавил движение инопланетян

--- projects/games/inviders/js/inviders.js
+++ projects/games/inviders/js/inviders.js
@@ -1,5 +1,8 @@
 let canvas = document.getElementById("game");
 const CTX = canvas.getContext("2d");
+
+console.log(canvas);
+
 const settings = {
     bulletSize: 2,
     aliensRows: ['white', 'yellow', 'purple', 'pink'],
@@ -10,6 +13,19 @@ class Game {
         this.ship = new Ship('ship');
         this.aliens = [];
         this.bullets = [];
+        
+        this.aliensSpeed = {
+            x: 2,
+            y: 9
+        }
+
+        this.aliensBlock = {
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0
+        }
+        this.moveDown = false;
 
         let y = 0;
 
@@ -17,6 +33,8 @@ class Game {
             let x = 0;
             let alien = null;
             let tmp = [];
+            
+            this.aliensBlock.right = 0;
 
             for(let i = 0;i < settings.aliensColumns; i++) {
                 alien = new Alien(row);
@@ -25,11 +43,15 @@ class Game {
                 x += alien.width() + 4;
 
                 tmp.push(alien);
+
+                this.aliensBlock.right += alien.width() + 4;
             }
 
             y += alien.height() + 6;
 
             this.aliens.push(tmp);
+
+            this.aliensBlock.bottom += y;
         });
     }
 
@@ -67,32 +89,64 @@ class Game {
 
         game.drawAliens();
         game.ship.draw();
-        game.checkAlens();
+        game.checkAliens();
         game.drawBullets();
+        game.moveAliens();
 
         requestAnimationFrame(game.update);
     }
 
-    checkAlens() {
-        let alients = [];
+    moveAliens() {
+        if(this.aliensBlock.right > canvas.clientWidth || this.aliensBlock.left < 0) {
+            this.aliensSpeed.x *= -1;
+            this.moveDown = true;
+        } 
+
+        if(this.aliens) {
+            this.aliens.forEach((row) => {
+                row.forEach((alien) => {
+                    let x = alien.position.x + this.aliensSpeed.x;
+                    let y = alien.position.y;
+                    
+                    if(this.moveDown) {
+                        y += this.aliensSpeed.y;
+                    }
+
+                    alien.setPosition(x, y);
+                });
+            });
+        }
+
+        this.aliensBlock.left += this.aliensSpeed.x;
+        this.aliensBlock.right += this.aliensSpeed.x;
+
+        if(this.moveDown) {
+            this.aliensBlock.bottom += this.aliensSpeed.y;
+            this.moveDown = false;
+        }
+    }
+
+    checkAliens() {
+        let aliens = [];
         
         this.aliens.forEach((row) => {
             row.forEach((alien) => {
-                alients.push(alien);
+                aliens.push(alien);
             })
         });
 
         this.bullets.forEach((bullet) => {
-            alients.forEach((alien) => {
-                if(bullet.position.x > alien.position.x && bullet.position.x <= alien.position.x + alien.width() && bullet.position.y < alien.position.y + alien.height() && !checked) {
+            aliens.find((alien) => {
+                if(bullet.position.x > alien.position.x && bullet.position.x <= alien.position.x + alien.width() && bullet.position.y < alien.position.y + alien.height() && !alien.destroyed && !bullet.destroyed) {
                     bullet.destroyed = true;
                     alien.destroyed = true;
 
-                    let bulletIndex = this.bullets.indexOf(bullet);
-                    this.bullets = this.bullets.splice(bulletIndex, 1);
+                    return alien;
                 }
             });
         });
+
+        this.bullets = this.bullets.filter((bullet) => !bullet.destroyed);
     }
 }
 
